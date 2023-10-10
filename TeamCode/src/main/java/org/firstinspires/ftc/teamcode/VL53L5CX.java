4b6bcb3db89b2e23c40fc12dc13c2c4923ed94f4
Vivaan S <72049995+Vivaan-crypto@users.noreply.github.com>
2023-10-09 22:37:51 -0400

    Some more code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VL53L5CX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VL53L5CX.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.app.backup.RestoreObserver;
+
 import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
 import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
 import com.qualcomm.robotcore.hardware.configuration.annotations.I2cDeviceType;
 import com.qualcomm.robotcore.util.TypeConversion;
+import static com.qualcomm.hardware.stmicroelectronics.VL53L0X.*;
+
 
 /**
  * Guide to write driver: https://github.com/FIRST-Tech-Challenge/ftcrobotcontroller/wiki/Writing-an-I2C-Driver
@@ -13,12 +17,14 @@ import com.qualcomm.robotcore.util.TypeConversion;
  */
 
 @I2cDeviceType
-@DeviceProperties(name = "VL53L5 ToF Sensor", description = "This is the time of flight sensor from STM" ,xmlTag = "VL53L5")
+@DeviceProperties(name = "VL53L5 ToF Sensor", description = "This is the time of flight sensor from STM", xmlTag = "VL53L5")
 
 public class VL53L5CX extends I2cDeviceSynchDevice<I2cDeviceSynch> {
     public enum Register {
+        FIRST(0),
         WRITE(0x52),
-        READ(0x53);
+        READ(0x53),
+        LAST(READ.bVal);
 
         public int bVal;
 
@@ -26,23 +32,15 @@ public class VL53L5CX extends I2cDeviceSynchDevice<I2cDeviceSynch> {
             this.bVal = bVal;
         }
     }
-    public final static I2cAddr ADDRESS_I2C_DEFAULT = I2cAddr.create8bit(0x52);
 
-    @Override
-    public Manufacturer getManufacturer()
-    {
-        return Manufacturer.STMicroelectronics;
-    }
-
-    @Override
-    protected synchronized boolean doInitialize()
-    {
-        return true;
-    }
-
-    @Override
-    public String getDeviceName() {
-        return "STMicroelectronics VL53L5 ToF Sensor";
+    protected void setOptimalReadWindow() {
+        // Sensor registers are read repeatedly and stored in a register. This method specifies the
+        // registers and repeat read mode
+        I2cDeviceSynch.ReadWindow readWindow = new I2cDeviceSynch.ReadWindow(
+                Register.FIRST.bVal,
+                Register.LAST.bVal - Register.FIRST.bVal + 1,
+                I2cDeviceSynch.ReadMode.REPEAT);
+        this.deviceClient.setReadWindow(readWindow);
     }
 
     public VL53L5CX(I2cDeviceSynch deviceClient, boolean deviceClientIsOwned) {
@@ -54,8 +52,27 @@ public class VL53L5CX extends I2cDeviceSynchDevice<I2cDeviceSynch> {
         this.deviceClient.engage();
     }
 
+    public final static I2cAddr ADDRESS_I2C_DEFAULT = I2cAddr.create8bit(0x52);
 
+    @Override
+    public Manufacturer getManufacturer() {
+        return Manufacturer.STMicroelectronics;
+    }
 
+    @Override
+    protected synchronized boolean doInitialize() {
+        return true;
+    }
+
+    @Override
+    public String getDeviceName() {
+        return "STMicroelectronics VL53L5 ToF Sensor";
+    }
+
+    protected void writeShort(final Register reg, short value)
+    {
+        deviceClient.write(reg.bVal, TypeConversion.shortToByteArray(value));
+    }
     protected short read(Register reg) {
 
         return TypeConversion.byteArrayToShort(deviceClient.read(reg.bVal, 2));
